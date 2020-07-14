ade3047c9368a5043bc8919c746a6a2a480349b8
ryo-tokuda <ryo.tokuda@di-v.co.jp>
2020-07-14 13:02:48 +0900

    既読、未読の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,25 +4,25 @@ class PostsController < ApplicationController
   @posts = Post.all.order(id: "DESC")
  end
 
- def new
- end
 
  def create
-  Post.create(content: params[:content])
-  redirect_to action: :index
- end
+  post = Post.create(content: params[:content], checked: false)
+  redirect_to action: :index  
+  render json:{ post: post }
+end
 
  def checked
+
    post = Post.find(params[:id])
    #設定したURLパラメーターから、既読したメモのidが渡されるように設定するので、そのidを使用して該当するレコードを取得
-   if post.checked
+   if post.checked then
       post.update(checked: false)
    else
-     post.update(checked: true)
+      post.update(checked: true)
    end
-      
-    item = Post.find(params[:id])
-    render json: { post: item }
+   #checkedアクションで返却したitemは、XHR.response.postで取得
+     item = Post.find(params[:id])
+     render json: { post: item }
    #更新したレコードをitem = Post.find(params[:id])で取得し直し、render json:{ post: item }でJSON形式（データ）として返却
   end
 end
